0323e7a813241a6e2af92cb95116881e0bb66cdf
Danny Tseng <github email address>
2019-06-05 16:27:38 -0700

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(time) {
+  let greeting = time;
+  let hour = parseInt(time.split(":")[0]);
+  if (hour < 12) {
+    greeting = "Good Morning";
+  } else if (hour >= 12 && hour <= 17) {
+    greeting = "Good Afternoon";
+  } else if (hour > 17) {
+    greeting = "Good Evening";
+  }
+  return greeting;
+
+}
+
+function displayMessage(message) {
+  document.getElementById("greeting").innerText = message;
+}
