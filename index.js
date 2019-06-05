57c027c5f42c5141e8f307fbd733b5353b9682eb
Hanna Engel <hengel@alumni.berklee.edu>
2019-06-05 11:58:37 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* Given Code, don't edit */
 
 function handleClick(e) {
+ 
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
 }
@@ -9,3 +10,25 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+
+function greet(string){
+  let t = parseInt(string.split('').slice(0,2).join(''))
+  if (t < 12){
+    return 'Good Morning';
+  }
+  else if (t < 17 ) {
+    return'Good Afternoon';
+  }
+  else{
+    return 'Good Evening';
+  }
+}
+
+function displayMessage(f){
+  const message = document.getElementById('greeting')
+  console.log(message)
+  message.textContent = f
+}
+
+  
